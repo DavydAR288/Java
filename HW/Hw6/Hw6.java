f277590d369d5be057e386980817414f0b0e33c2
Davyd.AR <9766288@mail.ru>
2023-06-23 22:42:42 +0300

    HW6 correct

--- HW/Hw6/Hw6.java
+++ HW/Hw6/Hw6.java
@@ -1,39 +1,51 @@
 package HW.Hw6;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.HashSet;
+import java.util.Objects;
 
 public class Hw6 {
-    private HashMap<String, Set<String>> phoneBook;
-
-    public Hw6() {
-        phoneBook = new HashMap<>();
-    }
-
-    public void add(String name, String phone) {
-        if (!phoneBook.containsKey(name)) {
-            phoneBook.put(name, new HashSet<>());
-        }
-        phoneBook.get(name).add(phone);
-    }
-
-    public void print() {
-        List<Map.Entry<String, Set<String>>> list =
-                new ArrayList<>(phoneBook.entrySet());
-        Collections.sort(list, (o1, o2) -> o2.getValue().size() - o1.getValue().size());
-        for (Map.Entry<String, Set<String>> entry : list) {
-            System.out.printf("%s: %s%n", entry.getKey(), entry.getValue());
-        }
-    }
-
     public static void main(String[] args) {
-        Hw6 phoneBook = new Hw6();
-        phoneBook.add("Дмитрий", "+7-495-655-08-97");
-        phoneBook.add("Мария", "+7-499-612-33-77");
-        phoneBook.add("Дмитрий", "+7-999-945-00-75");
-        phoneBook.add("Мария", "+7-495-655-08-97");
-        phoneBook.add("Дмитрий", "+7-909-012-88-66");
-        phoneBook.add("Татьяна", "+7-999-123-23-32");
-        phoneBook.add("Тимур", "+7-495-122-45-11");
-        phoneBook.print();
+        Set<Integer> set1 = new Set();
+        set1.add(555);
+        set1.remove(123);
+        set1.size();
+        set1.isEmpty();
+
+        Iterator<Integer> it = set1.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        };
+
     }
+}
+
+
+class Set<E>{
+    private HashMap<E, Object> set = new HashMap<>();
+
+    private static final Object VALL = new Object();
+
+    public boolean add(E tell) {
+        return set.put(tell, VALL) == null;
+    }
+
+    public boolean remove(E tell) {
+        return set.remove(tell) == VALL;
+    }
+
+    public int size() {
+        return set.size();
+    }
+
+    public boolean isEmpty(){
+        return set.isEmpty();
+    }
+
+    public Iterator<E> iterator() {
+        return set.keySet().iterator();
+    }
+
+
 }
